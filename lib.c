aef6b4640d2c3d44595d91f7e3ddb5ec7bd093de
pt9999 <18054486+pt9999@users.noreply.github.com>
2024-12-06 13:21:18 +0900

    ffi: update lib.c

--- lib.c
+++ lib.c
@@ -1,16 +1,14 @@
 #include <netdb.h>
-//#include <sys/types.h>
-//#include <unistd.h>
-//#include <time.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 
 // TODO: use getaddrinfo(3)?
 
+// TODO: use hstrerror() and return host lookup error message
 int minilib_net_resolve_ipaddress_v4(const char* hostname, uint8_t *out_addr, size_t* io_addr_size)
 {
-    printf("hostname=%s\n", hostname);
+    //printf("hostname=%s\n", hostname);
     struct hostent ret = { 0 };
     struct hostent *result = NULL;
     char buf[65536];
@@ -19,18 +17,20 @@ int minilib_net_resolve_ipaddress_v4(const char* hostname, uint8_t *out_addr, si
     size_t addr_size = 0;
 
     int err = gethostbyname_r(hostname, &ret, buf, sizeof(buf), &result, &h_errnop);
-    printf("err=%d result=%p h_errnop=%d\n", err, result, h_errnop);
-    herror("herror:");
+    //printf("err=%d result=%p h_errnop=%d\n", err, result, h_errnop);
+
     if (err != 0) {
+        // TODO: use hstrerror() and return host lookup error message when err == EAGAIN(11)
+        herror("herror:");
         return err;
     }
     if (result != NULL) {
         int h_length = result->h_length;
         char** h_addr_list = result->h_addr_list;
-        printf("h_length=%d h_addr_list=%p\n", h_length, h_addr_list);
+        //printf("h_length=%d h_addr_list=%p\n", h_length, h_addr_list);
         for (size_t i = 0; h_addr_list[i] != NULL; i++) {
             uint8_t* addr = h_addr_list[i];
-            printf("h_addr[%lu]=%d.%d.%d.%d\n", i, addr[0], addr[1], addr[2], addr[3]);
+            //printf("h_addr[%lu]=%d.%d.%d.%d\n", i, addr[0], addr[1], addr[2], addr[3]);
             if (addr_size + 3 < max_addr_size) {
                 out_addr[addr_size++] = addr[0];
                 out_addr[addr_size++] = addr[1];
@@ -38,7 +38,7 @@ int minilib_net_resolve_ipaddress_v4(const char* hostname, uint8_t *out_addr, si
                 out_addr[addr_size++] = addr[3];
             }
         }
-        printf("buf=%s\n", buf);
+        //printf("buf=%s\n", buf);
     }
     *io_addr_size = addr_size;
 
